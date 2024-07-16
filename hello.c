c02978ac433018566594d3eabb6c5f440a2c2183
Sital-Dangaura <sitaldangaura@gmail.com>
2024-07-16 10:48:46 +0545

    Initial Commit

---
+++ hello.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main ()
+
+{
+    int age =10;
+    float weight =56.78f;
+    double height =123.4;
+    char gender ='m';
+    char name []="john doe";
+
+
+printf ("Age:%d\n",age );
+printf("Weight :%f\n", weight);
+printf("Height :%1f\n", height );
+printf("Gender:%c\n", gender );
+printf("Name :%s\n",name );
+
+
+
+return 0;
+
+}
